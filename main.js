c7d6882ed4c1e8f54fa81474814689d15ddc4497
Mousedlf <dlfcaroline@gmail.com>
2022-10-10 13:00:12 +0200

    c'est nul

---
+++ main.js
@@ -0,0 +1,20 @@
+let word = "bonjour"
+let word2 = "Michel"
+let phrase = word + word2
+
+
+word4 = ['banane', 'carotte', 'haricot','patate','radis']
+let word5 = word4.splice(2,1, )
+
+console.log(word5)
+
+const monParagraphe = document.querySelector(".rouge")
+
+setInterval(()=>{
+    monParagraphe.classList.toggle("bleu")
+} ,1000)
+
+
+
+let mots = ['Salut','Hi',"Hola"]
+
